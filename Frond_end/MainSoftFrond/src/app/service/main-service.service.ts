5ba4e92a277c390cdfbc09adbc330aa1cdf21aad
rodriguez23 <ksrodriguez23@misena.edu.co>
2019-11-30 16:01:49 -0500

    Se agrega vista de detalle del usuario y correccion de css de seleccion

--- Frond_end/MainSoftFrond/src/app/service/main-service.service.ts
+++ Frond_end/MainSoftFrond/src/app/service/main-service.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
-import { tap, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
