6fea96ceed4ccd2ef39850743739077607b297fc
rodriguez23 <ksrodriguez23@misena.edu.co>
2019-11-30 12:11:50 -0500

    Se agregan servicios en el front en para despues desarrollar la suscripcion en cada vista

--- Frond_end/MainSoftFrond/src/app/service/main-service.service.ts
+++ Frond_end/MainSoftFrond/src/app/service/main-service.service.ts
@@ -1,16 +1,58 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from 'selenium-webdriver/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { tap, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MainServiceService {
 
-  urlUsuarios = 'http://localhost:3000/';
-  urlRegistroOferta='http://localhost:3000/';
-  urlRoles='http://localhost:3000/';
-  urlAplicarOferta='http://localhost:3000/';
+  urlUsuarios = 'http://localhost:3000/api/v1.0/usuario';
+  urlRegistroOferta = 'http://localhost:3000/api/v1.0/registroOferta';
+  urlAplicarOferta = 'http://localhost:3000/api/v1.0/aplicarOferta';
+  urlRoles = 'http://localhost:3000/api/v1.0/roles';
 
 
   constructor(public http: HttpClient) { }
+
+  getUsuarios() {
+    return this.http.get(this.urlUsuarios);
+  }
+
+  // Ofertas
+  getRegistroOfertas() {
+    return this.http.get(this.urlRegistroOferta);
+  }
+
+  setRegistroOfertas(data: any) {
+    return this.http.post(this.urlRegistroOferta, data);
+  }
+
+  getRegistroOfertasId(id: string) {
+    return this.http.get(`${this.urlRegistroOferta}/${id}`);
+  }
+
+  putRegistroOfertas(data: any) {
+    return this.http.put(this.urlRegistroOferta, data);
+  }
+
+  deleteRegistroOfertas(id: string) {
+    return this.http.delete(`${this.urlRegistroOferta}/${id}`);
+  }
+
+  //Alicar Oferta
+
+  getAplicacionOferta() {
+    return this.http.get(this.urlAplicarOferta);
+  }
+
+  setAplicacionOferta(data: any){
+    return this.http.post(this.urlAplicarOferta, data);
+  }
+
+
+
+
+
 }
